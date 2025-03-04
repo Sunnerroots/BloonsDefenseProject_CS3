f9a532566b560ce496c9f6252bb88dad8a7c52de
Deepak Murali <deepakmurali@Deepaks-MacBook-Air.local>
2025-03-04 11:47:48 -0600

    Starting template

---
+++ src/Enemy.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class Enemy {
+    private int x, y;
+    private int speed = 2;
+    private int width = 40, height = 40;
+    
+    public Enemy(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update() {
+        x += speed; // Moves enemy to the right
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, width, height);
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+// Player class to manage their curency
+public class Player {
+    private int health = 100;
+    private int gold = 500;
+
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.drawString("Health: " + health, 10, 20);
+        g.drawString("Gold: " + gold, 10, 40);
+    }
+}
