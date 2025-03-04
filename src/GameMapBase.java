4bacb7e979200dc9284bfbf0ab1436fe1702cfc3
SunrutM <sunrutneuro@gmail.com>
2025-03-04 11:46:53 -0600

    Flight Background

--- src/GameMapBase.java
+++ src/GameMapBase.java
@@ -1,2 +1,10 @@
-public class GameMapBase {
+import java.io.File;
+
+public class GameMapBase
+{
+    File bg;
+    public GameMapBase()
+    {
+        bg = new File("/Users/Macbook/Desktop/School_24-25/CS3/BloonsDefenseProject_CS3/FlightEmoji.jpg");
+    }
 }
