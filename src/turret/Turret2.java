6716317c6941ba437aadb32df8702eae69439d9e
Deepak Murali <deepakmurali@Deepaks-MacBook-Air.local>
2025-04-10 12:03:14 -0500

    Turret Refactored

---
+++ src/turret/Turret2.java
@@ -0,0 +1,7 @@
+package turret;
+
+public class Turret2 extends BaseTurret {
+    public Turret2(int x, int y) {
+        super(x, y, 20, 1, 10);
+    }
+}
---
+++ src/turret/Turret3.java
@@ -0,0 +1,7 @@
+package turret;
+
+public class Turret3 extends BaseTurret {
+    public Turret3(int x, int y) {
+        super(x, y, 20, 1, 10);
+    }
+}
