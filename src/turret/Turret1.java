93e63ac2905407a4d9c3631ef1eca38e102492a3
Deepak Murali <deepakmurali@Deepaks-MacBook-Air.local>
2025-04-30 10:56:11 -0500

    Make screen with information

--- src/turret/Turret1.java
+++ src/turret/Turret1.java
@@ -2,6 +2,6 @@ package turret;
 
 public class Turret1 extends BaseTurret {
     public Turret1(int x, int y) {
-        super(x, y, 430, 800, 15,"src/images/turrets/p1Final.png");
+        super(x, y, 400, 1000, 15,"src/images/turrets/p1Final.png");
     }
 }
--- src/turret/Turret3.java
+++ src/turret/Turret3.java
@@ -3,6 +3,6 @@ package turret;
 public class Turret3 extends BaseTurret
 {
     public Turret3(int x, int y) {
-        super(x, y, 1500, 15, 50, "src/images/turrets/p3Final.png");
+        super(x, y, 600, 600, 50, "src/images/turrets/p3Final.png");
     }
 }
